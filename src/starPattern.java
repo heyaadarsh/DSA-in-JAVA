3dc9886c33aa4a7b5d5ecf30d6ef7c67bf0f0360
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2023-04-24 19:01:46 +0530

    Pattern printing using System.out.print function

---
+++ src/starPattern.java
@@ -0,0 +1,16 @@
+/*
+* Print following pattern:
+*     ****
+*     ***
+*     **
+*     *
+* */
+
+public class starPattern {
+    public static void main(String[] args){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
