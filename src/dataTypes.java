af975ef746dbc5c79c2192955f6dddf7fa2848b3
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2023-04-24 19:32:44 +0530

    Data types in JAVA

---
+++ src/dataTypes.java
@@ -0,0 +1,37 @@
+public class dataTypes {
+    public static void main(String[] args){
+
+        // byte having size of 1 byte and stores value from -128 to 127
+        byte a = 1;
+        System.out.println(a);
+
+        // short - 2 bytes
+        short b = 2;
+        System.out.println(b);
+
+        // char - 2 bytes
+        char c = '@';
+        System.out.println(c);
+
+        // boolean - 1 byte
+        boolean d = true;
+        System.out.println(d);
+
+        // int - 4 bytes
+        int e = 5;
+        System.out.println(e);
+
+        // long - 8 bytes
+        long f = 1654614896545L;
+        System.out.println(f);
+
+        // float - 4 bytes
+        float g = 45.23F;
+        System.out.println(g);
+
+        // double - 8 bytes
+        double h = 789.23658;
+        System.out.println(h);
+    }
+
+}
