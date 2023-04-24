b4f45a3ec6c7eb6dd34d9eadcd9a2160f335ea2a
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2023-04-24 19:15:53 +0530

    Variables in JAVA

---
+++ src/variables.java
@@ -0,0 +1,18 @@
+public class variables {
+    public static void main(String[] args){
+        int a = 10; // variable 'a' of 'int' data type having value 10
+        int b = 5;
+        String name = "AYATI";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+        a = 25; // Variable value overriding
+        System.out.println(a);
+
+        a = b; // Assigning one variable value to another
+        System.out.println(a);
+
+    }
+}
