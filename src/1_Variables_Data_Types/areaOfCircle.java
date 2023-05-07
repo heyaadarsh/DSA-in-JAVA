7c40473121b0eec3948e69c6ce27dcaa7c7320d1
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2023-05-07 18:25:18 +0530

    Area of the circle

---
+++ src/1_Variables_Data_Types/areaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class areaOfCircle{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println("Area: " + area);
+    }
+}
