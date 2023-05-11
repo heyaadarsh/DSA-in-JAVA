a8f9b37ad9667be5f03e4b0075a2d1de61e0819c
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2023-05-11 15:22:02 +0530

    Operators | Arithmetic Operators

---
+++ src/Operators/arithmeticOperators.java
@@ -0,0 +1,21 @@
+package Operators;
+
+public class arithmeticOperators {
+    public static void main(String args[]){
+        int a = 20;
+        int b = 10;
+
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int rem = a % b;
+
+
+        System.out.println("Sum of the numbers are: " + sum);
+        System.out.println("Sub of the numbers are: "+ sub);
+        System.out.println("Mul of the numbers are: "+ mul);
+        System.out.println("Div of the numbers are: "+ div);
+        System.out.println("Rem of the numbers are: "+ rem);
+    }
+}
