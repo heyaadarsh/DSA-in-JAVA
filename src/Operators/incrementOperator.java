be32ce92f15af573e832a65468e2c997837f2adc
heyaadarsh <aadarsh0001.be21@chitkara.edu.in>
2023-05-11 15:35:38 +0530

    Operators | Increment Operator

---
+++ src/Operators/incrementOperator.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class incrementOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        int p = 10;
+        int q = p++;
+        System.out.println(p);
+        System.out.println(q);
+    }
+}
